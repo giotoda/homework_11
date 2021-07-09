75f92fac5b5f7e080f4345bfe89e2a813ce9c09f
giotoda <giotoda05@gmail.com>
2021-07-09 16:15:56 +0400

    create scrips file and add variables

---
+++ script.js
@@ -0,0 +1,26 @@
+//4.
+let x = 20;
+let y = 21;
+let z = Math.sqrt(x*x + y*y)
+
+//3.
+let x = 9;
+let y = 3.14*x*y;
+console.log('x',x)
+let z = 2*3.14*x*x;
+console.log('z',z)
+
+//5.
+let Buckwheat = prompt('write Buckwheat')
+let x = Buckwheat*Buckwheat*Buckwheat;
+console.log('cube Buckwheat',x)
+
+//2.
+let x = 10; 
+let x = 13.25; 
+let x = "hello"; 
+let x = "Script"; 
+let x = true;  
+let x = false; 
+let x = null;
+let x = undefined;
